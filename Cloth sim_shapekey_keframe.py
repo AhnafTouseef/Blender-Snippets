88cdc521319908d804e3654e3e5e99b23cee2414
AhnafTouseef <158799743+AhnafTouseef@users.noreply.github.com>
2024-07-01 22:40:53 +0600

    Update Cloth sim_shapekey_keframe.py
    Added previous shape key deleting code and modifier on/off code. refined function and way to utilize keyframing

--- Cloth sim_shapekey_keframe.py
+++ Cloth sim_shapekey_keframe.py
@@ -4,30 +4,45 @@ frame_current= bpy.data.scenes["Scene"].frame_current
 frame_start = bpy.data.scenes["Scene"].frame_start 
 frame_end = bpy.data.scenes["Scene"].frame_end
 
-for i in range(frame_end):
-    bpy.ops.object.modifier_apply_as_shapekey(keep_modifier=True, modifier="Cloth")
-    bpy.context.scene.frame_current = i+1
-
-
-bpy.context.object.active_shape_key_index = 1
-bpy.data.shape_keys["Key"].use_relative = False
-
-obj = bpy.context.object.data.shape_keys
-
-def insert_keyframe():
-    obj.keyframe_insert("eval_time")
+def insert_keyframe(frame):
+    obj = bpy.context.object.data.shape_keys
+    obj.keyframe_insert("eval_time", frame = frame)
     return 'Finished'
 
 
 def go_to_frame(frame):
     bpy.context.scene.frame_current = frame
     return 'Finished'
-    # Insert keyframe 
-go_to_frame(frame_start)
-insert_keyframe()
 
-go_to_frame(frame_end)
-bpy.data.shape_keys["Key"].eval_time = (frame_end*10)
-insert_keyframe()
-#bpy.ops.action.interpolation_type(type='LINEAR')
+    #Clean previous shape keys
+try:
+    bpy.ops.object.shape_key_remove(all=True, apply_mix=False)
+except Exception:
+    print('done')
 
+if bpy.context.object.modifiers["Cloth"].show_viewport == False:
+    bpy.context.object.modifiers["Cloth"].show_viewport = True
+
+
+    #Set up shape keys
+for i in range(frame_end):
+    bpy.ops.object.modifier_apply_as_shapekey(keep_modifier=True, modifier="Cloth")
+    bpy.context.scene.frame_current = i+1
+
+    #Set up shape key interpolation mode
+bpy.context.object.active_shape_key_index = 0
+bpy.data.shape_keys["Key"].use_relative = False
+
+
+def main():
+    insert_keyframe(frame_start)
+
+    bpy.data.shape_keys["Key"].eval_time = (frame_end*10)
+
+    insert_keyframe(frame_end)
+
+    bpy.context.object.modifiers["Cloth"].show_viewport = False
+    
+    go_to_frame(frame_current)
+
+main()
